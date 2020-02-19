2c4115f5cbd77a640f8c51a35cfdc9fad664f2ef
Piotr Kudrel <piotr.kudrel@deneblab.com>
2020-02-20 00:11:18 +0100

    Working version

--- index.js
+++ index.js
@@ -1,41 +1,19 @@
 const path = require("path");
 const crypto = require("crypto");
+const reg = require("./registryManager").getRegistry();
 const args = require("yargs").argv;
 const fs = require("fs");
 const fsp = require("fs").promises;
-const exeDir = path.dirname(process.execPath);
-const isExe = exeDir.endsWith("nodejs") ? false : true;
-console.log(`exeDir: ${exeDir}`);
-
-const puppeteerExtra = path.join(exeDir, "puppeteer-extra");
-const puppeteerStealthPlugin = path.join(
-  exeDir,
-  "puppeteer-extra-plugin-stealth"
-);
-const subPathToChromium = isExe
-  ? path.join(exeDir, "chromium")
-  : path.join(__dirname, "node_modules", "\\puppeteer\\.local-chromium");
-console.log(`subPathToChromium: ${subPathToChromium}`);
-const pathToChromium = path.join(
-  subPathToChromium,
-  "win64-722234\\chrome-win\\chrome.exe"
-);
-console.log(`pathToChromium: ${pathToChromium}`);
-const puppeteer = isExe ? require(puppeteerExtra) : require("puppeteer-extra");
-const StealthPlugin = isExe
-  ? require(puppeteerStealthPlugin)
+const puppeteer = reg.isExe ? require(reg.puppeteerExtraDir) : require("puppeteer-extra");
+const StealthPlugin = reg.isExe
+  ? require(reg.puppeteerStealthPluginDir)
   : require("puppeteer-extra-plugin-stealth");
 puppeteer.use(StealthPlugin());
 
-// const urlToProcess = process.argv[2];
-// console.log(`Work-dir: ${exeDir}`);
-// console.log(`Url to process: ${urlToProcess}`);
-// const uriIn = new URL(urlToProcess);
-
-async function processUrls(urlsAll, outDir) {
+async function processUrls(urlsAll, outDir, overrideResults) {
   const browser = await puppeteer.launch({
-    headless: false,
-    executablePath: pathToChromium
+    headless: reg.headless,
+    executablePath: reg.pathToChromium
   });
   var i = 0;
   var urls1 = urlsAll
@@ -54,6 +32,21 @@ async function processUrls(urlsAll, outDir) {
 
     try {
       const uriIn = new URL(urlToProcess);
+      var hash = crypto
+        .createHash("md5")
+        .update(uriIn.href)
+        .digest("hex");
+      const fileName = uriIn.host + "_" + hash + ".json";
+      const outFile =
+        (!outDir || outDir == undefined) == false && fs.existsSync(outDir)
+          ? path.join(outDir, fileName)
+          : fileName;
+
+      if (fs.existsSync(outFile) && overrideResults != true) {
+        console.log(`File: ${outFile} exists. Skipping ...`);
+        continue;
+      }
+
       await page.goto(uriIn);
       await page.waitFor(2000);
       const html = await page.content();
@@ -68,16 +61,6 @@ async function processUrls(urlsAll, outDir) {
       doc.DocumentInfo.UrlIn = uriIn;
       doc.DocumentInfo.UrlOut = uriOut;
 
-      var hash = crypto
-        .createHash("md5")
-        .update(uriIn.href)
-        .digest("hex");
-
-      const fileName = uriIn.host + "_" + hash + ".json";
-      const outFile =
-        (!outDir || outDir == undefined) == false && fs.existsSync(outDir)
-          ? path.join(outDir, fileName)
-          : fileName;
       await fsp.writeFile(outFile, JSON.stringify(doc));
       console.log(`File to save: ${outFile}`);
     } catch (error) {
@@ -96,7 +79,6 @@ async function getUrls(pathIn) {
     try {
       if (fs.existsSync(pathLocal)) {
         const data = await fsp.readFile(pathLocal);
-        console.log(`data: ${data}`);
         var arr1 = data.toString().split("\n");
         res.push(...arr1);
       }
@@ -108,21 +90,22 @@ async function getUrls(pathIn) {
 
   var items = [];
   if (pathIn == undefined) return items;
-  var p1 = path.join(__dirname, pathIn);
-  var r1 = await tryReadFile(p1);
+
+  var r1 = await tryReadFile(pathIn);
   items.push(...r1);
   return items.filter(s => (!s || s == undefined) == false).map(s => s.trim());
 }
 
 (async () => {
   var items = [];
-  var r1 = await getUrls(args.urlsFile);
+  var fullPath = path.resolve(args.urlsFile);
+  if (fs.existsSync(fullPath)) {
+    console.log(`File with urls: ${fullPath}`);
+    var r1 = await getUrls(fullPath);
+    items.push(...r1);
+  } else {
+    items.push(args._[0]);
+  }
 
-  items.push(...r1);
-  items.push(args._[0]);
-  console.log(`data: ${args.urlsFile}`);
-  console.log(`data: ${args.outDir}`);
-  console.log(`data: ${args._[0]}`);
-
-  await processUrls(items, args.outDir);
+  await processUrls(items, args.outDir, args.overrideResults);
 })();
---
+++ registryManager.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const fs = require("fs");
+const exeDir = path.dirname(process.execPath);
+const isExe = exeDir.endsWith("nodejs") ? false : true;
+const executeDir = isExe ? path.dirname(process.execPath) : __dirname;
+const configFile = path.join(executeDir, "config.json");
+
+if (fs.existsSync(configFile)) {
+  console.log(`Use config file: ${configFile}`);
+}
+
+const config = fs.existsSync(configFile)
+  ? JSON.parse(fs.readFileSync(configFile, "utf8"))
+  : { headless: true, pathToChromium: null };
+
+const subPathToChromium = isExe
+  ? path.join(exeDir, "chromium")
+  : path.join(__dirname, "node_modules", "\\puppeteer\\.local-chromium");
+const pathToChromium = path.join(
+  subPathToChromium,
+  "win64-722234\\chrome-win\\chrome.exe"
+);
+function getRegistry() {
+  return {
+    headless: config.headless,
+    executeDir: executeDir,
+    isExe: isExe,
+    pathToChromium: config.pathToChromium
+      ? config.pathToChromium
+      : pathToChromium,
+    puppeteerExtraDir: path.join(executeDir, "puppeteer-extra"),
+    puppeteerStealthPluginDir: path.join(
+      executeDir,
+      "puppeteer-extra-plugin-stealth"
+    )
+  };
+}
+
+module.exports.getRegistry = getRegistry;
