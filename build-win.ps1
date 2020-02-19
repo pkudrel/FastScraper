6e2621e1832bb9b5d641cc51e7cb6d3483aa1db1
Piotr Kudrel <piotr.kudrel@deneblab.com>
2020-02-19 15:11:24 +0100

    Fix build

--- build-win.ps1
+++ build-win.ps1
@@ -1,1 +1,6 @@
 pkg --out-path dist -t  node12-win-x64 .
+Copy-Item .\node_modules\puppeteer-extra -destination dist -recurse -Force
+Copy-Item .\node_modules\puppeteer-extra-plugin-stealth -destination dist -recurse -Force
+mkdir dist\chromium -Force
+Copy-Item .\node_modules\puppeteer\.local-chromium\* -destination dist\chromium -recurse -Force
+
